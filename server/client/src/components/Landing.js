b1163edaed5220337101a8719f5cc07b8cd629dc
Alex Webb <alexwebbb@protonmail.com>
2018-05-29 01:07:23 -0500

    added client files

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Astrological Affinities!
+      </h1>
+      Landing page
+    </div>
+  );
+};
+
+export default Landing;
